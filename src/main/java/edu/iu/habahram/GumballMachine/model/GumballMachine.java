ce779cd133bffe685afd56fdd721310d2a6ba071
Flora, Spencer Mark <spmflora@iu.edu>
2024-04-24 03:27:47 -0400

    added crank, eject, and dispense methods

--- src/main/java/edu/iu/habahram/GumballMachine/model/GumballMachine.java
+++ src/main/java/edu/iu/habahram/GumballMachine/model/GumballMachine.java
@@ -35,16 +35,66 @@ public class GumballMachine implements IGumballMachine {
 
     @Override
     public TransitionResult ejectQuarter() {
-        //TODO
-        return null;
+        boolean finish = false;
+        String m = "";
+        if(state.equalsIgnoreCase(HAS_QUARTER)){
+            m = "Quarter Ejected Successfully";
+            state = NO_QUARTER;
+            finish = true;
+        }
+        else if(state.equalsIgnoreCase(NO_QUARTER)){
+            m = "There is no quarter to eject";
+        }
+        else if(state.equalsIgnoreCase(SOLD_OUT)){
+            m = "There is no quarter to eject because the machine is sold out";
+        }
+        else if(state.equalsIgnoreCase(SOLD)){
+            m = "Please do not try to shortchange the machine";
+        }
+        return new TransitionResult(finish, m, state, count);
     }
 
     @Override
     public TransitionResult turnCrank() {
-        //TODO
-        return null;
+        boolean finish = false;
+        String m = "";
+        if(state.equalsIgnoreCase(HAS_QUARTER)){
+            m = "Turning Crank";
+            state = SOLD;
+            finish = true;
+        }
+        else if(state.equalsIgnoreCase(NO_QUARTER)){
+            m = "There is no quarter to turn the crank";
+        }
+        else if(state.equalsIgnoreCase(SOLD_OUT)){
+            m = "There is no quarter to crank because the machine is sold out";
+        }
+        else if(state.equalsIgnoreCase(SOLD)){
+            m = "Please do not try to shortchange the machine";
+        }
+        return new TransitionResult(finish, m, state, count);
     }
 
+    public TransitionResult dispense(){
+        boolean finish = false;
+        String m = "";
+        if(state.equalsIgnoreCase(HAS_QUARTER)){
+            m = "Please turn the crank to continue";
+        }
+        else if(state.equalsIgnoreCase(NO_QUARTER)){
+            m = "Please insert a quarter then turn the crank to continue";
+        }
+        else if(state.equalsIgnoreCase(SOLD_OUT)){
+            m = "There is no ball to dispense because the machine is sold out";
+        }
+        else if(state.equalsIgnoreCase(SOLD)){
+            m = "dispensing ball";
+            releaseBall();
+            state = NO_QUARTER;
+            finish = true;
+        }
+        return new TransitionResult(finish, m, state, count);
+    }
     @Override
     public void changeTheStateTo(GumballMachineState name) {
 
